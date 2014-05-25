4d7abc3aff361f9ecfda13c13581ac71fe45af92
unknown <aimery.thomas@outlook.com>
2014-05-25 21:44:40 +0200

    session_20140525

---
+++ app.js
@@ -0,0 +1,144 @@
+const path = require('path')
+    , express = require('express')
+    , connect = require('express/node_modules/connect')
+    , cookie = require('express/node_modules/cookie')
+    , app = express()
+    , http = require('http')
+    , server = http.createServer(app)
+    , port = process.env.PORT || 1337
+    , sessionStore = new express.session.MemoryStore({ reapInterval: 60000 * 10 })
+    , sessionSecret = "some private string"
+    ;
+
+// Public API: distinct app (Express) and actual server (HttpServer)
+module.exports = { app: app, server: server };
+
+/** Configuration */
+app.configure(function() {
+  this.engine('ejs', require('ejs-locals'));
+  this.set('views', path.join(__dirname, 'views'));
+  this.set('view engine', 'ejs');
+  this.use(express.static(path.join(__dirname, '/public')));
+  // Allow parsing cookies from request headers
+  this.use(express.cookieParser());
+  // Session management
+  // Internal session data storage engine, this is the default engine embedded with connect.
+  // Much more can be found as external modules (Redis, Mongo, Mysql, file...). look at "npm search connect session store"
+  this.use(express.session({ "secret": sessionSecret, "store": sessionStore }));
+  // Allow parsing form data
+  this.use(express.bodyParser());
+});
+app.configure('development', function(){
+  this.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+app.configure('production', function(){
+  this.use(express.errorHandler());
+});
+
+/** Routes */
+app.get('/session-index', function (req, res, next) {
+  // Increment "index" in session
+  req.session.index = (req.session.index || 0) + 1;
+  // View "session-index.ejs"
+  res.render('session-index', {
+    "index": req.session.index,
+    "sessId": req.sessionID
+  });
+});
+
+/** Middleware for limited access */
+function requireLogin (req, res, next) {
+  if (req.session.username) {
+    // User is authenticated, let him in
+    next();
+  } else {
+    // Otherwise, we redirect him to login form
+    res.redirect("/login");
+  }
+}
+/** Home page (requires authentication) */
+app.get('/', [requireLogin], function (req, res, next) {
+  res.render('index');
+});
+/** Login form */
+app.get("/login", function (req, res) {
+  // Show form, default value = current username
+  res.render("login", { "username": req.session.username, "pass": req.session.pass, "error": null });
+});
+
+app.post("/login", function (req, res) {
+  //var motpasse ="";
+  var options = { "username": req.body.username, "pass": req.body.pass,"error": null };
+  if (!req.body.username) {
+    options.error = "User name is required";
+    res.render("login", options);
+  } else if (req.body.username == req.session.username) {
+    // User has not changed username, accept it as-is
+    res.redirect("/");
+  } else if (!req.body.username.match(/^[a-zA-Z0-9\-_]{3,}$/)) {
+    options.error = "User name must have at least 3 alphanumeric characters";
+    res.render("login", options);
+  } else {
+ // Validate if username is free
+    sessionStore.all(function (err, sessions) {
+      if (!err) {
+        var found = false;
+        for (var i=0; i<sessions.length; i++) {
+          var session = JSON.parse(sessions[i]);
+          if (session.username == req.body.username) {
+            err = "User name already used by someone else";
+            found = true;
+            break;
+          }
+        }
+      }
+      if (err) {
+        options.error = ""+err;
+        res.render("login", options);
+      } else {
+        var sql = 'SELECT * FROM projet_web.users WHERE name = '+ connection.escape(req.body.username);
+        connection.query(sql, function(err, rows,fields) {
+          if (err) throw err;
+          console.log(rows[0].password);
+          var motpasse = rows[0].password; 
+          //connection.end();
+          console.log(motpasse);
+          if (req.body.pass == motpasse){
+            console.log('yes');
+            req.session.username = req.body.username;
+            res.redirect("/");
+          }else{
+            console.log('bad!!');
+            options.error = "Already used or other error I do not know sorry"
+            res.render("login", {"username" : "", "pass" : ""});
+          }
+        });
+      }
+    });
+  }
+});
+
+/** Start server */
+if (!module.parent) {
+  server.listen(port, function () {
+    console.log('Listening', this.address());
+  })
+}
+
+
+//attention, ne fonction que pour Aimery car il faut avoir crée la bdd correspondante
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
